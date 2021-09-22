2f79db10c0b082b14a74573e874995bcc2252660
Santiago Góngora <sgongora@fing.edu.uy>
2021-09-22 03:44:48 -0300

    Build matrix from lab.json in order to run dijkstra's algorithm

--- fat-tree-generator/graph.py
+++ fat-tree-generator/graph.py
@@ -1,25 +1,23 @@
+import json, sys, os
 # Python program for Dijkstra's single
 # source shortest path algorithm. The program is
 # for adjacency matrix representation of the graph
 
-# Library for INT_MAX
-import sys
-
-
-# This code is contributed by Divyanshu Mehta
+# This code is contributed by Divyanshu Mehta 
+#(https://www.geeksforgeeks.org/python-program-for-dijkstras-shortest-path-algorithm-greedy-algo-7/)
 
 class Graph():
 
-    def __init__(self, vertices):
+    def __init__(self, vertices,names):
         self.V = vertices
         self.graph = [[0 for column in range(vertices)]
                     for row in range(vertices)]
+        self.names = names
 
-    def printSolution(self, dist):
-        # print("Vertex tDistance from Source")
+    def printSolution(self, dist,path_dict,src):
         for node in range(self.V):
-            # print(node, "t", dist[node])
-            print(f'Al nodo {node} se llega con costo {dist[node]}')
+            print(f'Origen: {self.names[src]}\nDestino: {self.names[node]}')
+            print(f'Camino: {[self.names[x] for x in path_dict[node]]} (costo {dist[node]})\n')
 
     # A utility function to find the vertex with
     # minimum distance value, from the set of vertices
@@ -47,6 +45,7 @@ class Graph():
         dist[src] = 0
         sptSet = [False] * self.V
 
+        path_dict = {k:[] for k in range(self.V)}
         for cout in range(self.V):
 
             # Pick the minimum distance vertex from
@@ -65,45 +64,54 @@ class Graph():
             for v in range(self.V):
                 if self.graph[u][v] > 0 and    sptSet[v] == False and dist[v] > (dist[u] + self.graph[u][v]):
                     dist[v] = dist[u] + self.graph[u][v]
+                    path_dict[v] = [step for step in path_dict[u]] + [v]
 
-        self.printSolution(dist)
+        self.printSolution(dist,path_dict,src)
 
-def build_fat_tree_graph():
-    # Formulas used:
-    #   Number of Planes: k_leaf / r
-    #   Number of PoDs: (k_leaf + k_top) / r
-    #   Number of Leaf per PoD: k_top
-    #   Number of ToP per PoD: k_leaf
-    #   Number of ToFs: k_top * (k_leaf / r)
-  
-    fat_tree = Graph(20)
-    for row_number,row in enumerate(fat_tree.graph):
-        if row_number in range(8) and row_number % 2 == 0:
-            fat_tree.graph[row_number][row_number+8] = 1
-            fat_tree.graph[row_number+8][row_number] = 1
-            fat_tree.graph[row_number][row_number+8+1] = 1
-            fat_tree.graph[row_number+8+1][row_number] = 1  
-        elif row_number in range(8) and row_number % 2 == 1:
-            fat_tree.graph[row_number][row_number+8-1] = 1
-            fat_tree.graph[row_number+8-1][row_number] = 1
-            fat_tree.graph[row_number][row_number+8] = 1
-            fat_tree.graph[row_number+8][row_number] = 1
-        elif row_number in range(8,16) and row_number % 2 == 0:
-            fat_tree.graph[row_number][16] = 1
-            fat_tree.graph[16][row_number] = 1
-            fat_tree.graph[row_number][17] = 1
-            fat_tree.graph[17][row_number] = 1  
-        elif row_number in range(8,16) and row_number % 2 == 1:
-            fat_tree.graph[row_number][18] = 1
-            fat_tree.graph[18][row_number] = 1
-            fat_tree.graph[row_number][19] = 1
-            fat_tree.graph[19][row_number] = 1  
-        for i in range(20):
-            if row_number!=i and fat_tree.graph[row_number][i]==0:
-                fat_tree.graph[row_number][i] = 99
-    return fat_tree       
+def build_fat_tree_from_lab(path_to_lab):
+    node_neighbours = {}
+    tof = []
+    spine = []
+    leaf = []
+    server = []
+    
+    # Get every node's name from lab.json and the name of their neighbours as well
+    with open(os.path.join(path_to_lab,'lab.json')) as json_file:
+        lab_json = json.load(json_file)
+        for tof_node in lab_json['aggregation_layer']:
+            node_neighbours[tof_node] = []
+            tof.append(tof_node)
+            for neighbour in lab_json['aggregation_layer'][tof_node]['neighbours']:
+                node_neighbours[tof_node] += [neighbour[0]]
+        for pod in lab_json['pod']:
+            for node in lab_json['pod'][pod]:
+                node_neighbours[node] = []
+                if node.startswith('spine'): spine.append(node)
+                elif node.startswith('leaf'): leaf.append(node)
+                elif node.startswith('server'): server.append(node)
+                for neighbour in lab_json['pod'][pod][node]['neighbours']:
+                    node_neighbours[node] += [neighbour[0]]
 
+    # Each node in the topology must have an id
+    names = {}
+    for number, node in enumerate(leaf + spine + tof + server):
+        names[number] = node
+    
+    # Generate adjacency matrix representation of the topology
+    # Graph object also has the id-name dictionary (self.names)
+    fat_tree = Graph(len(names.keys()), names)
+    for row_number in range(len(fat_tree.graph)):
+        for column_number in range(len(fat_tree.graph)):
+            if names[row_number] == names[column_number]:
+                fat_tree.graph[row_number][column_number] = 0
+            elif names[column_number] in node_neighbours[names[row_number]]:
+                fat_tree.graph[row_number][column_number] = 1
+            else:
+                fat_tree.graph[row_number][column_number] = sys.maxsize
+    
+    return fat_tree
 
 if __name__ == '__main__':
-    k4 = build_fat_tree_graph() 
+    print('=== Dijkstra desde el nodo 0 ===')
+    k4 = build_fat_tree_from_lab('fat_tree_2_2_1+1_1_1+bgp')
     k4.dijkstra(0)

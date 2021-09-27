1f363889a076c3c33e2a181845f848a0330b8fb6
Santiago Góngora <sgongora@fing.edu.uy>
2021-09-27 19:43:42 -0300

    Shortest path routing with dijkstra's algorithm

--- fat-tree-generator/graph.py
+++ fat-tree-generator/graph.py
@@ -62,7 +62,7 @@ class Graph():
             # distance is greater than new distance and
             # the vertex in not in the shortest path tree
             for v in range(self.V):
-                if self.graph[u][v] > 0 and    sptSet[v] == False and dist[v] > (dist[u] + self.graph[u][v]):
+                if self.graph[u][v] > 0 and sptSet[v] == False and dist[v] > (dist[u] + self.graph[u][v]):
                     dist[v] = dist[u] + self.graph[u][v]
                     path_dict[v] = [step for step in path_dict[u]] + [v]
 
@@ -81,30 +81,39 @@ def build_fat_tree_from_lab(path_to_lab):
         for tof_node in lab_json['aggregation_layer']:
             node_neighbours[tof_node] = []
             tof.append(tof_node)
-            for neighbour in lab_json['aggregation_layer'][tof_node]['neighbours']:
-                node_neighbours[tof_node] += [neighbour[0]]
+            interfaces = lab_json['aggregation_layer'][tof_node]['interfaces']
+            for interface in interfaces:
+                if interface['collision_domain'] != 'lo':
+                    for neighbour in interface['neighbours']:
+                        node_neighbours[tof_node] += [(neighbour[0],neighbour[1])]
+        print(node_neighbours)
         for pod in lab_json['pod']:
             for node in lab_json['pod'][pod]:
                 node_neighbours[node] = []
                 if node.startswith('spine'): spine.append(node)
                 elif node.startswith('leaf'): leaf.append(node)
                 elif node.startswith('server'): server.append(node)
-                for neighbour in lab_json['pod'][pod][node]['neighbours']:
-                    node_neighbours[node] += [neighbour[0]]
+                interfaces = lab_json['pod'][pod][node]['interfaces']
+                for interface in interfaces:
+                    if interface['collision_domain'] != 'lo':
+                        for neighbour in interface['neighbours']:
+                            node_neighbours[node] += [(neighbour[0],neighbour[1])]
 
     # Each node in the topology must have an id
     names = {}
     for number, node in enumerate(leaf + spine + tof + server):
         names[number] = node
     
+    
     # Generate adjacency matrix representation of the topology
     # Graph object also has the id-name dictionary (self.names)
     fat_tree = Graph(len(names.keys()), names)
     for row_number in range(len(fat_tree.graph)):
         for column_number in range(len(fat_tree.graph)):
+            neighbour_names = [x[0] for x in node_neighbours[names[row_number]]]
             if names[row_number] == names[column_number]:
                 fat_tree.graph[row_number][column_number] = 0
-            elif names[column_number] in node_neighbours[names[row_number]]:
+            elif names[column_number] in neighbour_names:
                 fat_tree.graph[row_number][column_number] = 1
             else:
                 fat_tree.graph[row_number][column_number] = sys.maxsize

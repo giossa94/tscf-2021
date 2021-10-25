d6744dc4c037593f09ed50fe107e777cabf86508
Santiago Góngora <sgongora@fing.edu.uy>
2021-10-25 09:45:29 -0300

    Create topology config file reading from command prompt

--- fat-tree-generator/build_and_run_topology.py
+++ fat-tree-generator/build_and_run_topology.py
@@ -1,11 +1,12 @@
 from src.utils import create_fat_tree, read_config
-import subprocess
-import os
+from build_config_json import build_config
+import subprocess,os,sys
 
-params = read_config('config.json')
+# params = read_config('config.json')
+print(f'Creating Fat Tree with K={sys.argv[1]}. The number of planes is 2 by default.')
 
+params = build_config(int(sys.argv[1]))
 config, output_dir, lab_dir = create_fat_tree(params, os.path.abspath('.')) #, args.dir, args.name, args.kube_net)
-
 
 os.chdir(lab_dir)
 subprocess.run(["kathara", "lstart", "--noterminals"])
